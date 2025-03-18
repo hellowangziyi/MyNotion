ad9a0fba6aba8dae7038a0215177f1b274dfe30a
wangziyi <ziyi.wang@shopee.com>
2025-03-18 18:53:30 +0800

    Initial Project

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,13 @@
+const MarketingLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div className="h-full dark:bg-[#1F1F1F]">
+      <main className="h-full pt-40">{children}</main>
+    </div>
+  );
+};
+
+export default MarketingLayout;
