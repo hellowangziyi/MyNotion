e07fbbc11307d0be161ed35628dc41a35365c33e
wangziyi <ziyi.wang@shopee.com>
2025-03-24 11:14:55 +0800

    feat: add shadcn ui

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,10 +1,12 @@
 import { Heading } from "./_components/Heading";
+import { Button } from "@/components/ui/button";
 
 const MarketingPage = () => {
   return (
     <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
+        <Button size="lg">click me</Button>
       </div>
     </div>
   );
